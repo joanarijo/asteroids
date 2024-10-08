c2580c98f99c240a7c72080a0f1d10cb81f7dce2
joanarijo <joanarijodesign@gmail.com>
2024-10-08 16:15:46 +0100

    add destruction

--- main.py
+++ main.py
@@ -39,6 +39,11 @@ def main():
             if asteroid.collision(player):
                 print("Game over!")
                 sys.exit()
+                
+            for shot in shots:
+                if asteroid.collision(shot):
+                    shot.kill()
+                    asteroid.kill()
         
         screen.fill((0, 0, 0))
         
