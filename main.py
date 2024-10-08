dd813c1d2132c793c3d0218906370b3538acdd6d
joanarijo <joanarijodesign@gmail.com>
2024-10-08 16:32:13 +0100

    add splitting

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def main():
             for shot in shots:
                 if asteroid.collision(shot):
                     shot.kill()
-                    asteroid.kill()
+                    asteroid.split()
         
         screen.fill((0, 0, 0))
         
